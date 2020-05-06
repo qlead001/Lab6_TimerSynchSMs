6aeaba0c1b62bd3611a2d15fc90a4fff7f13056e
qlead001 <qlead001@ucr.edu>
2020-05-06 08:55:16 -0700

    Update period and use timeContinue

--- source/main.c
+++ source/main.c
@@ -102,7 +102,7 @@ int main(void) {
     DDRA = 0x00; PORTA = 0xFF;
     DDRB = 0xFF; PORTB = 0x00;
     /* Insert your solution below */
-    TimerSet(10);
+    TimerSet(100);
     TimerOn();
     state = Start;
     while (1) {
